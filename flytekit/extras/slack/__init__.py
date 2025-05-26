c72de7ba74674bdae48e450fad0840ac4d137c39
Abhay Vardhan <abhay.vardhan@gmail.com>
2025-05-26 02:00:12 +0000

    feat: register SlackConnector on import
    Co-authored-by: Genie <genie@cosine.sh>

--- flytekit/extras/slack/__init__.py
+++ flytekit/extras/slack/__init__.py
@@ -1,3 +1,4 @@
 from .task import SlackNotificationTask
+from .connector import SlackConnector  # Register the connector on import
 
-__all__ = ["SlackNotificationTask"]
+__all__ = ["SlackNotificationTask", "SlackConnector"]
